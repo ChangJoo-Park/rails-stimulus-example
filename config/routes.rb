5dbd1694379a8b20d2554794daa962f452c343e5
ChangJoo Park <pcjpcj2@gmail.com>
2019-02-03 21:59:13 +0900

    Add controllers for examples

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   get 'example/clipboard'
   get 'example/content_loader'
   get 'example/slideshow'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
